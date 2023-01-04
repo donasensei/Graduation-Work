70359b455c29a977795eba53fdbfcc96e6a7dc9a
Wonseok Do <donasu0417@gmail.com>
2023-01-04 23:04:00 +0900

    Init Project
    언리얼 5.1.0 프로젝트 최초 생성 & 커밋

---
+++ Source/Graduation_Works/Graduation_WorksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Graduation_WorksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRADUATION_WORKS_API AGraduation_WorksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
